3e3e26229384da8dfab92485431c7c82a6e9b988
user <user@DESKTOP-COJ9858>
2023-10-27 17:43:16 +0900

    1회차

--- naverWebtoon/src/webtoonPage/Webtoon.java
+++ naverWebtoon/src/webtoonPage/Webtoon.java
@@ -1,7 +1,39 @@
 package webtoonPage;
 
+import java.io.*;
+import java.util.*;
+
+
 public class Webtoon {
 	private String title; // 웹툰 제목
 	private String author; // 작가명
-	private int rating; // 평점
+	private double rating; // 평점
+	private String desciption; // 웹툰 소개글
+	public Webtoon() {
+		
+	}
+	public Webtoon(String title, String author, double rating) {
+		this.title = title;
+		this.author = author;
+		this.rating = rating;
+	}
+	public double getRating() {
+		return rating;
+	}
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+	public String getDesciption() {
+		return desciption;
+	}
+	public void setDesciption(String desciption) {
+		this.desciption = desciption;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	
 }
---
+++ naverWebtoon/src/webtoonPage/WebtoonManager.java
@@ -0,0 +1,8 @@
+package webtoonPage;
+
+import java.util.List;
+
+public class WebtoonManager {
+	private Webtoon webtoon;
+	private List<WebtoonEpisode> episodes;
+}
