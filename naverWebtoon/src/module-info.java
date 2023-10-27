387065d21e7558608e9563909e843dc72f2d2018
user <user@DESKTOP-COJ9858>
2023-10-27 15:05:26 +0900

    test commit

---
+++ naverWebtoon/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module naverWebtoon {
+}
---
+++ naverWebtoon/src/webtoonPage/Webtoon.java
@@ -0,0 +1,7 @@
+package webtoonPage;
+
+public class Webtoon {
+	private String title; // 웹툰 제목
+	private String author; // 작가명
+	private int rating; // 평점
+}
