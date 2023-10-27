7b09b480559dd9ab70d6ce881bee1536a32104ca
Myeongsin <85742119+Myeongsin05@users.noreply.github.com>
2023-10-27 11:23:10 +0900

    Add files via upload

---
+++ Creators/src/Post/Main.java
@@ -0,0 +1,50 @@
+package Post;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+//		String path ="C:\\b01_javaexp\\workplace3\\Creators\\src\\";
+//		String fname = "stipulation.txt";
+//		int lineToRead = 4;
+//		try {
+//			FileReader fin = new FileReader(path+fname);
+//			BufferedReader bfr = new BufferedReader(fin);
+//			String line;
+//			int currentLine = 0;
+//			StringBuilder sbf = new StringBuilder();
+//			while((line = bfr.readLine())!=null && currentLine < lineToRead) {
+//				sbf.append(line).append("\n");
+//				currentLine++;
+//			}
+//			
+//			fin.close();
+//			System.out.println("이용약관: "+sbf.toString());
+//			System.out.println("이용약관 전문을 보시겠습니까? 보시길 원하면 Y를 눌러주세요.");
+//			Scanner sc = new Scanner(System.in);
+//			String input = sc.nextLine();
+//			if(input.equals("Y")) {
+//				System.out.println("이용약관 전문:");
+//				while(true) {
+//					FileReader fin2 = new FileReader(path+fname);
+//					StringBuffer sbf2 = new StringBuffer();
+//					
+//				}
+//			}
+//		} catch (FileNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}catch(IOException e) {
+//			
+//		}
+		
+		
+
+	}
+
+}
